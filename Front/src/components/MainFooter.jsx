7ca7e75d9f4b36446bcc4c60f4b89ef60dd9f8de
DaviDemarqui <davi_demarqui@outlook.com>
2021-12-14 08:09:34 -0300

    little changes

--- Front/src/components/MainFooter.jsx
+++ Front/src/components/MainFooter.jsx
@@ -5,9 +5,9 @@ function MainFooter() {
     return (
         <div className="main-footer">
             <ul className='m-footer'> 
-                <li><a href="?"><FontAwesomeIcon icon={['fab', 'facebook-square']} /></a></li>
-                <li><a href="?"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a></li>
-                <li><a href="?"><FontAwesomeIcon icon={['fab', 'github-square']} /></a></li>
+                <li><a href="?" className='facebook-icon'><FontAwesomeIcon icon={['fab', 'facebook-square']} /></a></li>
+                <li><a href="?" className='linkedin-icon'><FontAwesomeIcon icon={['fab', 'linkedin']} /></a></li>
+                <li><a href="?" className='github-icon'><FontAwesomeIcon icon={['fab', 'github-square']} /></a></li>
                 <p>DaviDemarqui 2022</p>
             </ul>
         </div>
