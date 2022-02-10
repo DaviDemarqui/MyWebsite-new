5404353df02431ddce25d2c6fdf7effd64b07aea
DaviDemarqui <davipersonal@protonmail.com>
2022-02-10 17:54:27 -0300

    more little changes

--- Front/src/components/Container01.jsx
+++ Front/src/components/Container01.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import me from '../img/me.jpg';
 
 function Container01() {
     return (
-        <div className="container-01" id='home'>
+        <div className="container-01">
+            <div id='home'></div>
             <h3 className="hello-C1">Hello World<span>_</span></h3>
             <br />
             <h1>I'm Davi Demarqui</h1>
@@ -10,6 +12,9 @@ function Container01() {
             <h3>Self taught software developer from Brazil guided by the desire to evolve and innovate.</h3>
             <br />
             <h4 className='ddb0'>Full Stack Software Engineer</h4>
+            <div className="image">
+                <img src={me} alt="me with a computer" className='me' />
+            </div>
         </div>
     )
 }
